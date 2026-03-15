6b2bef386510a953bd307fbf622e1de299697c21
Gabriel Novalski <biell.gb4@gmail.com>
2026-03-14 23:10:28 -0300

    Create enum_ops.h

---
+++ include/mtl/enum_ops.h
@@ -0,0 +1,34 @@
+///-----------------------------------------------------------------------------
+/// MarineTelematics Template Library               - (c) 2026 Marine Telematics
+///-----------------------------------------------------------------------------
+///
+/// @file enum_ops.h
+/// @author Gabriel Novalski (gabriel@marinetelematics.com.br)
+/// @date 12-03-2026
+///
+///-----------------------------------------------------------------------------
+
+#ifndef MTL_ENUM_OPS_H
+#define MTL_ENUM_OPS_H
+
+#include <cstdint>
+
+#define MTL_ADD_ENUM_OPS(E)                                                             \
+    constexpr E operator|(const E a, const E b)                                           \
+    {                                                                                     \
+        return static_cast<E>(static_cast<size_t>(a) | static_cast<size_t>(b));           \
+    }                                                                                     \
+    constexpr E operator&(const E a, const E b)                                           \
+    {                                                                                     \
+        return static_cast<E>(static_cast<size_t>(a) & static_cast<size_t>(b));           \
+    }                                                                                     \
+    constexpr E operator^(const E a, const E b)                                           \
+    {                                                                                     \
+        return static_cast<E>(static_cast<size_t>(a) ^ static_cast<size_t>(b));           \
+    }                                                                                     \
+    constexpr E  operator~(const E a) { return static_cast<E>(~static_cast<size_t>(a)); } \
+    constexpr E &operator|=(E &a, E b) { return a = a | b; }                              \
+    constexpr E &operator&=(E &a, E b) { return a = a & b; }                              \
+    constexpr E &operator^=(E &a, E b) { return a = a ^ b; }
+
+#endif
